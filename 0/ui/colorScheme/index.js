9d5645d45aeb5671efff1d634b5d8454ffbedb2c
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-24 16:09:07 -0400

    Create index.js

---
+++ 0/ui/colorScheme/index.js
@@ -0,0 +1,8 @@
+// © 2021 Gavin Morrow
+// EasyJS UI
+
+export default {
+	set: light => localStorage.colorScheme = light ? "1" : "",
+	reset: () => localStorage.colorScheme = matchMedia("prefers-color-scheme: light") ? "1" : "",
+	
+};
