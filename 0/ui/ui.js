4ec2ae34a9e60ac5d6bfad17d4516894227a541e
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-24 14:54:07 -0400

    Update ui.js

--- 0/ui/ui.js
+++ 0/ui/ui.js
@@ -1,13 +1,10 @@
 // © 2021 Gavin Morrow
 // EasyJS UI
 
+import center from "./center";
+
 const ui = {
-	center: element => {
-		// Centers on full screen, must have a position of fixed, absolute, or relative.
-		element.style.top = (innerHeight - element.offsetHeight) / 2;
-		element.style.left = (innerWidth - element.offsetWidth) / 2;
-		return element;
-	},
+	center,
 };
 
 export default ui;
