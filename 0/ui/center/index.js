304085ac53a77c13073a0c221839a42c27bc86a8
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-24 16:23:47 -0400

    Update index.js

--- 0/ui/center/index.js
+++ 0/ui/center/index.js
@@ -3,5 +3,7 @@
 
 export default element => {
 	// Centers on full screen, must have a position of fixed, absolute, or relative.
+	element.style.top = `${(innerHeight - element.offsetHeight) / 2}px`;
+	element.style.left = `${(innerWidth - element.offsetWidth) / 2}px`;
 	return element;
 };
