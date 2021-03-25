f947104ab2d652ea9b6cf52df5d76854c4dec99d
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-25 12:52:13 -0400

    Update index.js

--- 0/ui/colorScheme/index.js
+++ 0/ui/colorScheme/index.js
@@ -4,5 +4,7 @@
 export default {
 	set: light => localStorage.colorScheme = light ? "1" : "",
 	reset: () => localStorage.colorScheme = matchMedia("prefers-color-scheme: light") ? "1" : "",
-	
+	setColors: (bg, navBg, cardBg, buttonBg, divideBg, text, subText) => {
+
+	},
 };
