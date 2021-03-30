64af0eaccf5d21a7be5105b994eed09a481cce76
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-30 11:28:10 -0400

    Update index.js

--- 0/ui/colorScheme/index.js
+++ 0/ui/colorScheme/index.js
@@ -1,10 +1,43 @@
 // © 2021 Gavin Morrow
 // EasyJS UI
 
+let bg = "";
+let navBg = "";
+let cardBg = "";
+let buttonBg = "";
+let divideBg = "";
+let text = "";
+let subText = "";
+
 export default {
 	set: light => localStorage.colorScheme = light ? "1" : "",
 	reset: () => localStorage.colorScheme = matchMedia("prefers-color-scheme: light") ? "1" : "",
-	setColors: (bg, navBg, cardBg, buttonBg, divideBg, text, subText) => {
+	setColors: (colors) => {
+		bg = colors.bg || bg;
+		navBg = colors.navBg || navBg;
+		cardBg = colors.cardBg || cardBg;
+		buttonBg = colors.buttonBg || buttonBg;
+		divideBg = colors.divideBg || divideBg;
+		text = colors.text || text;
+		subText = colors.subText || subText;
 
+		return {
+			bg,
+			navBg,
+			cardBg,
+			buttonBg,
+			divideBg,
+			text,
+			subText,
+		};
 	},
+	getColors: () => ({
+		bg,
+		navBg,
+		cardBg,
+		buttonBg,
+		divideBg,
+		text,
+		subText,
+	}),
 };
