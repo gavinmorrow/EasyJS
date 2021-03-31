c5e03406f02d9b642c2461b76e5fc5c1b42e6ab6
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-31 13:33:54 -0400

    Update Docs

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,2 @@
 import colorScheme from "../0/ui/colorScheme/index.js";
 
-console.log(colorScheme);
-console.log(colorScheme.colors.light.bg);
-console.log(colorScheme.setColors({bg: "#000000"}));
-console.log(colorScheme.colors.light.bg);
