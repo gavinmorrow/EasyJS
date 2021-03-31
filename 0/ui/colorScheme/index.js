49ec2d7dbf2646775ddb09def3bfeea5f20ef6d5
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-31 13:17:36 -0400

    Update index.js

--- 0/ui/colorScheme/index.js
+++ 0/ui/colorScheme/index.js
@@ -1,52 +1,51 @@
 // © 2021 Gavin Morrow
 // EasyJS UI
 
-let bg = "";
-let navBg = "";
-let cardBg = "";
-let buttonBg = "";
-let divideBg = "";
-let text = "";
-let subText = "";
+const lightColors = {
+	bg: "#ffffff",
+	navBg: "#fefffe",
+	cardBg: "#efeeef",
+	buttonBg: "#e0e1e0",
+	divideBg: "#2c2c2c",
+
+	text: "#131012",
+	subText: "#333032",
+}
+const darkColors = {
+	bg: "#111111",
+	navBg: "#181818",
+	cardBg: "#303130",
+	buttonBg: "#383938",
+	divideBg: "#d3d3d3",
+
+	text: "#ecefed",
+	subText: "#cccfcd",
+}
 
 export default {
 	set: light => localStorage.colorScheme = light ? "1" : "",
 	reset: () => localStorage.colorScheme = matchMedia("prefers-color-scheme: light") ? "1" : "",
-	setColors: (colors) => {
-		bg = colors.bg || bg;
-		navBg = colors.navBg || navBg;
-		cardBg = colors.cardBg || cardBg;
-		buttonBg = colors.buttonBg || buttonBg;
-		divideBg = colors.divideBg || divideBg;
-		text = colors.text || text;
-		subText = colors.subText || subText;
+	setColors: (light = lightColors, dark = darkColors) => {
+		const colors = ["bg", "navBg", "cardBg", "buttonBg", "divideBg", "text", "subText"];
+		const setVar = (name, value) => getComputedStyle(document.documentElement).setProperty(`--easyjs-${name}`, value);
+		for (const color of colors) {
+			lightColors[color] = light[color] || lightColors[color];
+			darkColors[color] = light[color] || darkColors[color];
 
-		return {
-			bg,
-			navBg,
-			cardBg,
-			buttonBg,
-			divideBg,
-			text,
-			subText,
-		};
+			switch (localStorage.colorScheme) {
+				case "1":
+					setVar(color, lightColors[color]);
+					break;
+				case "":
+					setVar(color, darkColors[color]);
+					break;
+				default:
+					throw new Error(`EasyJS UI Color Scheme colorScheme property is invalid. \n\nValue: "${localStorage.colorScheme}"`);
+			}
+		}
 	},
 	getColors: () => ({
-		bg,
-		navBg,
-		cardBg,
-		buttonBg,
-		divideBg,
-		text,
-		subText,
+		light: lightColors,
+		dark: darkColors,
 	}),
-	setCSS: () => {
-		getComputedStyle(document.documentElement).setProperty("--bg", bg);
-		getComputedStyle(document.documentElement).setProperty("--nav-bg", navBg);
-		getComputedStyle(document.documentElement).setProperty("--card-bg", cardBg);
-		getComputedStyle(document.documentElement).setProperty("--button-bg", butt);
-		getComputedStyle(document.documentElement).setProperty("--divide-bg", divideBg);
-		getComputedStyle(document.documentElement).setProperty("--text", text);
-		getComputedStyle(document.documentElement).setProperty("--sub-text", subText);
-	}
 };
