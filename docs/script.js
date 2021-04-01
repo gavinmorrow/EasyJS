7b490f70be0c44077323910c6bbbf309d258f489
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-01 10:17:18 -0400

    Update Docs

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,5 @@
 import colorScheme from "../0/ui/colorScheme/index.js";
 
+const main = document.getElementById("main");
+const nav = document.getElementById("nav");
+main.style.width = `${innerWidth - nav.offsetWidth}px`;
