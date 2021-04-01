0bbfaf47786049e4d63e9805d06bd7eae12bfd5c
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-01 10:41:04 -0400

    Update Docs

--- docs/script.js
+++ docs/script.js
@@ -2,4 +2,17 @@ import colorScheme from "../0/ui/colorScheme/index.js";
 
 const main = document.getElementById("main");
 const nav = document.getElementById("nav");
-main.style.width = `${innerWidth - nav.offsetWidth}px`;
+const title = document.getElementById("title");
+const realign = () => main.style.width = `${innerWidth - nav.offsetWidth}px`;
+realign();
+addEventListener("resize", realign);
+
+
+const titleBoxShadowSet = () => {
+	if (main.scrollTop > 0)
+		title.style.boxShadow = "0 0 1vmin 0 var(--easyjs-text)";
+	else
+		title.style.boxShadow = "0 0 0.25vmin 0 var(--easyjs-text)";
+};
+titleBoxShadowSet();
+main.addEventListener("scroll", titleBoxShadowSet);
