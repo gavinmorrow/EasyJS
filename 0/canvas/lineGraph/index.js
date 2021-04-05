e439a405e32834646e1a6aa3f911adeffd857cb4
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-05 09:54:19 -0400

    Update index.js

--- 0/canvas/lineGraph/index.js
+++ 0/canvas/lineGraph/index.js
@@ -3,8 +3,40 @@
 
 const lineGraph = {
 	LineGraph: class {
-		constructor (canvas, x = 0, y = 0, points = []) {
+		constructor (canvas, x = 0, y = 0, points = [], draw = true) {
+			this.canvas = canvas;
+			this.ctx = this.canvas.getContext("2d");
 
+			this.x = x;
+			this.y = y;
+			this.points = points;
+
+			if (draw) this.draw();
+		}
+		draw () {
+			ctx.beginPath();
+			ctx.moveTo(this.x, this.y);
+			for (const point of this.points) {
+				ctx.lineTo(point.x, point.y);
+			}
+			ctx.stroke();
+		}
+		addPoint (point, redraw = true) {
+			this.points.push(point);
+			if (redraw) this.draw();
+		}
+		removePoint (index, y) {
+			if (y) {
+				const x = index;
+				for (const point of this.points) {
+					if (point.x === x && point.y === y) index = this.points.indexOf(point);
+				}
+			}
+
+			for (let i = index; i < this.points.length; i++) {
+				if (this.points[i+1]) this.points[i] = this.points[i+1];
+				else delete this.points[i];
+			}
 		}
 	}
 };
