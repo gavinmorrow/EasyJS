3bc282631d5b5fe2064a8a7d8d4fc45dffb2d9e2
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-05 10:22:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 // © 2021 Gavin Morrow
 // EasyJS
 
-const EasyJSVersion = class {
-	done = false;
+const EasyJSVersion = async (v = 0) => {
+	const EasyJSVersion = class {
+		done = false;
+	
+		constructor (v = 0) {
+			const awaiter = async name => await import(`./${v}/${name}/${name}.js`).then(r => r.default);
+	
+			(async () => {
+				const divisions = ["canvas", "math", "string", "ui"];
+	
+				for (const division of divisions) {
+					this[division] = await awaiter(division);
+				}
+	
+				this.done = true;
+	
+				return this;
+			}).bind(this)();
+		}
+	};
 
-	constructor (v = 0) {
-		const awaiter = async name => await import(`./${v}/${name}/${name}.js`).then(r => r.default);
+	const EasyJS = new EasyJSVersion(v);
 
-		(async () => {
-			const divisions = ["canvas", "math", "string", "ui"];
-
-			for (const division of divisions) {
-				this[division] = await awaiter(division);
-			}
-
-			this.done = true;
-
-			return this;
-		}).bind(this)();
-	}
+	const i = setInterval(() => {
+		console.log(EasyJS.done, EasyJS.done ? Date.now() : void(0));
+		if (EasyJS.done) {
+			clearInterval(i);
+			return EasyJS;
+		}
+	});
 }
 
 export default EasyJSVersion;
