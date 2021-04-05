dc7799e6ee807b1a5d9c4f75b836a87276fa2410
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-05 15:59:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const EasyJSVersion = async (v = 0) => {
 
 	do {
 		if (EasyJS.done) return EasyJS;
-		else await new Promise(resolve => setTimeout(resolve, 10));
+		else await new Promise(resolve => resolve);
 	}while (true);
 }
 
