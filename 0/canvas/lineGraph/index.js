b87448e074805516aac27128c87fdcf78446491c
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-05 13:52:13 -0400

    Update index.js

--- 0/canvas/lineGraph/index.js
+++ 0/canvas/lineGraph/index.js
@@ -13,8 +13,9 @@ const lineGraph = {
 
 			if (draw) this.draw();
 		}
-		draw () {
+		draw (clear = true) {
 			this.ctx.beginPath();
+			if (clear) this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 			this.ctx.moveTo(this.x, this.y);
 			for (const point of this.points) {
 				this.ctx.lineTo(point.x, point.y);
@@ -25,18 +26,76 @@ const lineGraph = {
 			this.points.push(point);
 			if (redraw) this.draw();
 		}
-		removePoint (index, y) {
-			if (y) {
-				const x = index;
-				for (const point of this.points) {
-					if (point.x === x && point.y === y) index = this.points.indexOf(point);
-				}
-			}
+		removePoint (...args) {
+			const next = (redraw, clear) => redraw ? this.draw(clear) : void 0;
 
-			for (let i = index; i < this.points.length; i++) {
-				if (this.points[i+1]) this.points[i] = this.points[i+1];
-				else delete this.points[i];
+			let first = false;
+			switch (typeof args[1]) {
+				case "number":
+					const [x = 0, y = 0, redraw1 = true, clear1 = undefined] = args;
+					let i;
+
+					first = true;
+
+					for (const point of this.points) {
+						if (point.x === x && point.y === y) {
+							i = this.points.indexOf(point);
+							break;
+						}
+					}
+
+					next(redraw1, clear1);
+				case "boolean":
+					const [index, redraw = true, clear = undefined] = first ? [i, redraw1, clear1] : args;
+
+					for (let i = index; i < this.points.length; i++) {
+						if (this.points[i+1]) this.points[i] = this.points[i+1];
+						else this.points.pop();
+					}
+
+					next(redraw1, clear1);
+					break;
+				default:
+					break;
 			}
+			
+			// if (typeof args[1] == "number") {
+			// 	const [x = 0, y = 0, redraw = true, clear = undefined] = args;
+
+			// 	for (const point of this.points) {
+			// 		if (point.x === x && point.y === y) {
+			// 			index = this.points.indexOf(point);
+			// 			break;
+			// 		}
+			// 	}
+
+			// 	next(redraw, clear);
+			// } else if (typeof args[1] == "boolean")  {
+			// 	const [index, redraw = true, clear = undefined] = args;
+
+			// 	for (let i = index; i < this.points.length; i++) {
+			// 		if (this.points[i+1]) this.points[i] = this.points[i+1];
+			// 		else this.points.pop();
+			// 	}
+
+			// 	next(redraw, clear);
+			// }
+
+			// if (typeof redraw === "number") {
+			// 	const x = index;
+			// 	const y = redraw;
+			// 	for (const point of this.points) {
+			// 		if (point.x === x && point.y === y) {
+			// 			index = this.points.indexOf(point);
+			// 			break;
+			// 		}
+			// 	}
+			// }
+
+			// for (let i = index; i < this.points.length; i++) {
+			// 	if (this.points[i+1]) this.points[i] = this.points[i+1];
+			// 	else this.points.pop();
+			// }
 		}
 	}
 };
