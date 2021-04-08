9817237a3bf2796785064a809a8cde9620bd6988
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-08 16:29:52 -0400

    Update Some Stuff

--- 0/math/math.js
+++ 0/math/math.js
@@ -1,11 +1,11 @@
-// © 2021 Gavin Morrow
 // EasyJS Math
 
+import random from "./random/index.js";
+import clamp from "./clamp/index.js";
+
 const math = {
-	// Min: Inclusive
-	// Max: Exclusive
-	random: (min, max, places = 10) => Math.floor((Math.random()*(max-min)+min)*10**places)/10**places,
-	clamp: (min, prefered, max) => Math.max(min, Math.min(prefered, max)),
+	random,
+	clamp,
 };
 
 export default math;
