16e1c3e9dcefefb8ab3e461e60e58cb5ff5ec3e6
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-13 10:08:12 -0400

    Update Docs
    Patch still needed (#main does not resize).

--- docs/script.js
+++ docs/script.js
@@ -3,8 +3,9 @@ import EasyJSVersion from "../main.js";
 const main = document.getElementById("main");
 const nav = document.getElementById("nav");
 const title = document.getElementById("title");
+const closeNav = document.getElementById("close");
 
-const realign = () => main.style.width = `${innerWidth - nav.offsetWidth}px`;
+const realign = () => main.style.width = `calc(${innerWidth}px - ${nav.offsetWidth}px)`;
 const titleBoxShadowSet = () => {
 	if (main.scrollTop > 0)
 		title.style.boxShadow = "0 0 1vmin 0 var(--easyjs-text)";
@@ -22,10 +23,10 @@ main.addEventListener("scroll", titleBoxShadowSet);
 main.style.paddingTop = `${pxToNum(getComputedStyle(title).fontSize) + pxToNum(getComputedStyle(title).paddingTop) + pxToNum(getComputedStyle(title).paddingBottom)}px`;
 
 (async () => {
-	const EasyJS = await EasyJSVersion();
+	const EasyJS = await EasyJSVersion(1);
 
 	const scrollUpdate = () => {
-		document.querySelector(`.nav-a:first-of-type`).classList.remove("here");
+		document.querySelector(`.nav-a:not(#close):first-of-type`).classList.remove("here");
 		for (const section of document.querySelectorAll(".section")) {
 			document.querySelector(`.nav-a[href*="${section.id}"]`).classList.remove("here");
 			if (section.getBoundingClientRect().bottom > 0) {
@@ -39,7 +40,7 @@ main.style.paddingTop = `${pxToNum(getComputedStyle(title).fontSize) + pxToNum(g
 					if (section == document.getElementById("main-inner").firstElementChild) {
 						// Nothing is in the range, go to the overview. 
 						document.getElementById("title-inner").textContent = "";
-						document.querySelector(`.nav-a:first-of-type`).classList.add("here");
+						document.querySelector(`.nav-a:not(#close):first-of-type`).classList.add("here");
 					}
 				}
 			}
@@ -52,8 +53,36 @@ main.style.paddingTop = `${pxToNum(getComputedStyle(title).fontSize) + pxToNum(g
 		a.addEventListener("click", () => {
 			scrollUpdate();
 			a.blur();
-		})
+		});
 	}
 
-	
+	for (const linkable of document.querySelectorAll(".linkable")) {
+		const linkableLink = document.createElement("a");
+		linkableLink.textContent = "¶";
+		linkableLink.classList.add("linkable-link");
+		linkableLink.href = `#${linkable.id || ""}`;
+		linkable.appendChild(linkableLink);
+
+		const showLink = () => linkableLink.style.opacity = "1"
+		const hideLink = () => linkableLink.style.opacity = "0";
+
+		linkable.addEventListener("mouseenter", showLink);
+		linkable.addEventListener("focus", showLink);
+
+		linkable.addEventListener("mouseleave", hideLink);
+		linkable.addEventListener("blur", hideLink);
+
+		hideLink();
+	}
+
+	closeNav.addEventListener("click", () => {
+		if (nav.getAttribute("data-closed") !== "") {
+			nav.setAttribute("data-closed", "");
+			console.log(nav.style.left = `calc(0px - ${nav.offsetWidth}px + ${closeNav.offsetWidth}px + ${getComputedStyle(closeNav).padding} * 2 + ${getComputedStyle(nav).padding})`);
+		}else {
+			nav.removeAttribute("data-closed");
+			nav.style.left = ``;
+		}
+		realign();
+	});
 })();
