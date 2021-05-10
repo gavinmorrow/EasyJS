a95d4fb7aa7a133b10addb6ced91b71d5316fdab
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-10 16:03:31 -0400

    Update index.js
    PATCH

--- 1/ui/popup/index.js
+++ 1/ui/popup/index.js
@@ -5,39 +5,39 @@ popupStyle.rel = "stylesheet";
 popupStyle.href = "https://gavinmorrow.github.io/EasyJS/1/ui/popup/style.css";
 document.head.appendChild(popupStyle);
 
-popupStyle.addEventListener("load", () => {
-	class Popup {
-		constructor (txt, ms = 5000, del = false) {
-			this.txt = txt;
-			this.ms = ms;
-			this.delete = del;
+class Popup {
+	constructor (txt, ms = 5000, del = false) {
+		this.txt = txt;
+		this.ms = ms;
+		this.delete = del;
 
-			this.popup = document.createElement("div");
-			this.popup.classList.add("easyjs-popup");
-			this.popup.innerHTML = this.txt;
+		this.popup = document.createElement("div");
+		this.popup.classList.add("easyjs-popup");
+		this.popup.innerHTML = this.txt;
 
-			this.wrapper = document.createElement("div");
-			this.wrapper.classList.add("easyjs-popup-wrapper");
+		this.wrapper = document.createElement("div");
+		this.wrapper.classList.add("easyjs-popup-wrapper");
+		this.wrapper.style.zIndex = "-1000";
+		this.wrapper.style.opacity = "0";
 
-			this.wrapper.appendChild(this.popup);
-			document.body.appendChild(this.wrapper);
-		}
-		show (ms = this.ms) {
-			this.wrapper.style.zIndex = "1000";
-			this.wrapper.style.opacity = "1";
-			setTimeout(this.hide.bind(this), ms);
-		}
-		hide (del = this.delete) {
-			this.wrapper.style.opacity = "0";
-			setTimeout(() => {
-				this.wrapper.style.zIndex = "-1000";
-				if (del) this.wrapper.delete();
-			}, 1000);
-		}
-		delete () {
-
-		}
+		this.wrapper.appendChild(this.popup);
+		document.body.appendChild(this.wrapper);
 	}
+	show (ms = this.ms) {
+		this.wrapper.style.zIndex = "1000";
+		this.wrapper.style.opacity = "1";
+		setTimeout(this.hide.bind(this), ms);
+	}
+	hide (del = this.delete) {
+		this.wrapper.style.opacity = "0";
+		setTimeout(() => {
+			this.wrapper.style.zIndex = "-1000";
+			if (del) this.wrapper.delete();
+		}, 1000);
+	}
+	delete () {
 
-	export default Popup;
-});
+	}
+}
+
+export default Popup;
