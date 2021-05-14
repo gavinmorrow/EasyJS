fec9db772d3346979849297a2050c0330463e128
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-14 18:15:12 -0400

    stuff

--- 1/cookies/cookieConsent/index.js
+++ 1/cookies/cookieConsent/index.js
@@ -1,6 +1,5 @@
 // EasyJS Cookies
 
-import colorScheme from "/EasyJS/1/ui/colorScheme/index.js";
 const elemStyle = document.createElement("link");
 elemStyle.rel = "stylesheet";
 elemStyle.href = "https://gavinmorrow.github.io/EasyJS/1/cookies/cookieConsent/style.css";
