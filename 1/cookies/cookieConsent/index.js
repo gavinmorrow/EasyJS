d40ad4ff6ffe273969694588d5220a66f45d5e12
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-14 09:00:50 -0400

    Stuff

--- 1/cookies/cookieConsent/index.js
+++ 1/cookies/cookieConsent/index.js
@@ -1,1 +1,12 @@
 // EasyJS Cookies
+
+const elemStyle = document.createElement("link");
+elemStyle.rel = "stylesheet";
+elemStyle.href = "https://gavinmorrow.github.io/EasyJS/1/cookies/cookieConsent/style.css";
+const cookieConsent = (txt = "By continuting to use this site, you consent to our use of cookies.") => {
+	const elem = document.createElement("div");
+	elem.innerHTML = txt;
+
+	const close = document.createElement("button");
+	close.innerHTML = "Ok";
+}
