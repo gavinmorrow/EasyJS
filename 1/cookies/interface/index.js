0e12cf6c3f205142ba731ddfe8557da0ca1da0e5
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-17 10:15:43 -0400

    Update index.js

--- 1/cookies/interface/index.js
+++ 1/cookies/interface/index.js
@@ -3,11 +3,21 @@
 class Cookie {
 	constructor (name, value = "", expires = "", path = "/") {
 		this.name = name;
-		this.value = value;
+		this.txt = value;
 		this.expires = expires;
 		this.path = path;
 
-		document.cookie = `${name}=${value}; expires=${expires}; path=${path}`;
+		this.value = this.txt;
+	}
+	get value () {
+		return this.txt;
+	}
+	set value (value) {
+		this.txt = value;
+		document.cookie = `${this.name}=${this.txt}; expires=${this.expires}; path=${this.path}`;
+	}
+	delete () {
+		document.cookie = `${this.name}=${this.txt}; expires=Fri, 13 Mar 2015 00:00:00 GMT; path=${this.path}`;
 	}
 };
 
