4e52ce51a92375bf88fdbd5c517476bb9c0ccbb2
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-17 10:03:29 -0400

    Update index.js

--- 1/cookies/interface/index.js
+++ 1/cookies/interface/index.js
@@ -1,7 +1,14 @@
 // EasyJS Cookies
 
 class Cookie {
-	
+	constructor (name, value = "", expires = "", path = "/") {
+		this.name = name;
+		this.value = value;
+		this.expires = expires;
+		this.path = path;
+
+		document.cookie = `${name}=${value}; expires=${expires}; path=${path}`;
+	}
 };
 
 export default Cookie;
