e02fd8fb72f1fffaeedc5f605f84706cbd3a41d7
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-20 13:59:52 -0400

    Update index.js

--- 1/cookies/cookie/index.js
+++ 1/cookies/cookie/index.js
@@ -1,7 +1,21 @@
 // EasyJS Cookies
 
 class Cookie {
-	static cookies = {};
+	static get get (name) {
+		const or = (a, b) => (a === undefined || a === null) ? b : a;
+		const getValue = name => {
+			const match = new RegExp(`${name}=([^;]*);`).exec(document.cookie);
+			if (match) return or(match[1], null);
+			else return null;
+		}
+		if (match) {
+			const value   = or(getValue(name), "");
+			const expires = or(getValue(`${name}-expires`), "");
+			const path    = or(getValue(`${name}-path`), "/");
+			return new Cookie(name, value, expires, path);
+		}
+		return null;
+	};
 	constructor (name, value = "", expires = "", path = "/") {
 		this.name = name;
 		this.txt = value;
@@ -17,9 +31,13 @@ class Cookie {
 	set value (value) {
 		this.txt = value;
 		document.cookie = `${this.name}=${this.txt}; expires=${this.expires}; path=${this.path}`;
+		document.cookie = `${this.name}-expires=${this.expires}; expires=${this.expires}; path=${this.path}`;
+		document.cookie = `${this.name}-path=${this.path}; expires=${this.expires}; path=${this.path}`;
 	}
 	delete () {
 		document.cookie = `${this.name}=${this.txt}; expires=Fri, 13 Mar 2015 00:00:00 GMT; path=${this.path}`;
+		document.cookie = `${this.name}-expires=${this.expires}; expires=Fri, 13 Mar 2015 00:00:00 GMT; path=${this.path}`;
+		document.cookie = `${this.name}-path=${this.path}; expires=Fri, 13 Mar 2015 00:00:00 GMT; path=${this.path}`;
 	}
 };
 
