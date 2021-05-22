33052506b2ceefb0b36be4a50b842ed6a1d30e8c
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-22 10:09:28 -0400

    Update index.js

--- 1/cookies/cookieConsent/index.js
+++ 1/cookies/cookieConsent/index.js
@@ -4,10 +4,9 @@ import Cookie from "../cookie/index.js";
 const elemStyle = document.createElement("link");
 elemStyle.rel = "stylesheet";
 elemStyle.href = "https://gavinmorrow.github.io/EasyJS/1/cookies/cookieConsent/style.css";
-if (document.querySelector(`link[href='${elemStyle.href}']`) != undefined) throw new Error("Cookie Consent has aready been called.");
 document.head.appendChild(elemStyle);
 const cookieConsent = (txt = "By continuting to use this site, you consent to our use of cookies.") => {
-	
+	if ((document.querySelectorAll(`link[href='${elemStyle.href}']`) || []).length > 0) throw new Error("Cookie Consent has aready been called.");
 	const cookie = Cookie.get("easyjs-cookie-consent") || new Cookie(`easyjs-cookie-consent`, "0");
 	if (cookie.value !== "1") {
 		try {
