f8ca52be504c6fc2c915cee5f2e997087169814d
gavinmorrow <gavinfmorrow@gmail.com>
2021-06-11 09:18:34 -0400

    PATCH

---
+++ 1/canvas/lineGraph/index.js
@@ -0,0 +1,78 @@
+// © 2021 Gavin Morrow
+// EasyJS Canvas
+
+const LineGraph = class {
+	constructor (canvas, x = 0, y = 0, points = [], draw = true) {
+		this.canvas = canvas;
+		this.ctx = this.canvas.getContext("2d");
+
+		this.points = points;
+		this.points.unshift({x, y});
+
+		if (draw) this.draw();
+	}
+	draw (clear = true) {
+		this.ctx.beginPath();
+		if (clear) this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.ctx.moveTo(this.points[0].x, this.points[0].y);
+		for (const point of this.points) {
+			this.ctx.lineTo(point.x, point.y);
+		}
+		this.ctx.stroke();
+	}
+	addPoint (point, index = this.points.length, redraw = true) {
+		this.points[index] = point;
+		if (redraw) this.draw();
+		return this.points.length;
+	}
+	removePoint (...args) {
+		const next = (redraw, clear) => redraw ? this.draw(clear) : void 0;
+
+		let removed;
+
+		let first = false;
+		switch (typeof args[1]) {
+			case "number":
+				const [x = 0, y = 0, redraw1 = true, clear1 = undefined] = args;
+				let i;
+
+				first = true;
+
+				i = this.getPoint(x, y);
+			case "boolean":
+				const [index, redraw = true, clear = undefined] = first ? [i, redraw1, clear1] : args;
+
+				for (let i = index; i < this.points.length; i++) {
+					if (this.points[i+1]) this.points[i] = this.points[i+1];
+					else removed = this.points.pop();
+				}
+
+				next(redraw, clear);
+			default:
+				return removed;
+		}
+	}
+	getPoint (arg1, arg2) {
+		if (arg2 != undefined) {
+			const x = arg1;
+			const y = arg2;
+
+			for (let i = 0; i < this.points.length; i++) {
+				const point = this.points[i];
+				if (point.x == x && point.y == y) return this.points.indexOf(point);
+			}
+
+			throw new Error("No such point.");
+		}
+		if (this.points[arg1] == undefined) throw new Error("Index out of bounds.");
+		return this.points[arg1];
+	}
+	changePoint(index, newValue, redraw = true, clear = undefined) {
+		this.points[index] = newValue;
+		if (redraw) this.draw(clear);
+
+		return this.points[index];
+	}
+};
+
+export default LineGraph;
---
+++ main.js
@@ -0,0 +1,31 @@
+// © 2021 Gavin Morrow
+// EasyJS
+
+const EasyJSVersion = async (v = 1) => {
+	const EasyJSVersion = class {
+		done = false;
+	
+		constructor (v) {
+			const awaiter = async name => await import(`./${v}/${name}/main.js`).then(r => r.default);
+	
+			(async () => {
+				const divisions = ["canvas", "cookies", "math", "string", "ui"];
+	
+				for (const division of divisions) {
+					this[division] = await awaiter(division);
+				}
+	
+				this.done = true;
+			}).bind(this)();
+		}
+	};
+
+	const EasyJS = new EasyJSVersion(v);
+
+	do {
+		if (EasyJS.done) return EasyJS;
+		else await new Promise(resolve => setTimeout(resolve, 10));
+	}while (true);
+}
+
+export default EasyJSVersion;
